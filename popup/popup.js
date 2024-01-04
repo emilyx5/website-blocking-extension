ee2c4f2b376dee7f44db027f3c20531ce895e128
Emily Xue <emily.xue31415@gmail.com>
2024-01-03 21:56:13 -0800

    Popup has cleaner layout/remove button

--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const blockButton = document.getElementById('blockButton');
     const websiteInput = document.getElementById('website');
-    const removeInput = document.getElementById('remove')
-    const removeButton = document.getElementById('removeButton')  
+    
 
     function updateBlockedList(blockedSites){
         blockedSitesList.innerHTML = '';
@@ -11,8 +10,26 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         else{
             blockedSites.forEach(function (site){
+              const deleteButton = document.createElement('span');
+              deleteButton.textContent = '-';
+              deleteButton.className = 'delete-button';
+    
+              deleteButton.addEventListener('click', function () {
+                const index = blockedSites.indexOf(site);
+                if (index !== -1) {
+                  chrome.storage.sync.get(['blockedSites'], function (result) {
+                    const blockedSites = result.blockedSites || [];
+                    blockedSites.splice(index, 1);
+                    chrome.storage.sync.set({ blockedSites }, function () {
+                      console.log(`${website} blocked.`);
+                      updateBlockedList(blockedSites);
+                    });
+                  });
+                }
+              });
                 const li = document.createElement("li");
                 li.textContent = site;
+                li.appendChild(deleteButton);
                 blockedSitesList.appendChild(li);
             });
         }
@@ -40,29 +57,6 @@ document.addEventListener('DOMContentLoaded', function () {
           }
         });
       }
-    });
-
-    removeButton.addEventListener('click', function (){
-      const website = removeInput.value.trim();
-      if(website){
-          chrome.storage.sync.get(['blockedSites'], function (result) {
-            const blockedSites = result.blockedSites || [];
-  
-            if (blockedSites.includes(website)){
-              const i = blockedSites.indexOf(website);
-              if (i > -1){
-                blockedSites.splice(i,1);
-                chrome.storage.sync.set({ blockedSites }, function () {
-                  console.log(`${website} removed.`);
-                  updateBlockedList(blockedSites);
-                });
-              }
-            } 
-            else {
-              console.log(`${website} is already blocked.`)
-            }
-        });
-      }
-    });
-    
+    }); 
   });
+
